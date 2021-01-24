210d8a04bd15ae5c5c96b09c8e9f398a325fd441
Greg Stead <gregstead@gmail.com>
2021-01-24 10:13:54 -0600

    Adds route to run on localhost

--- server.js
+++ server.js
@@ -15,11 +15,14 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // connect to mongoDB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-});
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/workout",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  }
+);
 
 // routes
 app.use(require("./routes"));
